2ba25b7843440474a489ad498df2a8530bf9b2cb
stadia <stadia1012@gmail.com>
2023-01-16 18:46:15 +0900

    계산기 calc5 Html 완성

--- StadiaJSP/src/main/java/site/stadiajsp/servlet/Calc5.java
+++ StadiaJSP/src/main/java/site/stadiajsp/servlet/Calc5.java
@@ -21,57 +21,67 @@ public class Calc5 extends HttpServlet{
 		response.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = response.getWriter();
-		Cookie[] cookies = request.getCookies();  // cookie는 배열로 선언
 		
-		String num_ = request.getParameter("num");
-		String op = request.getParameter("operator");
-		
-		int num = 0;
-		
-		if ( !num_.equals("") ) num = Integer.parseInt(num_);  // 빈문자열 검사.
+		out.write("<!DOCTYPE html>");
+		out.write("<html>");
+		out.write("<head>");
+		out.write("<meta charset=\"UTF-8\">");
+		out.write("<title>Stadia's Home</title>");
+		out.write("<style>");
+		out.write("input{");
+		out.write("	width: 50px;");
+		out.write("	height: 50px;");
+		out.write("}");
+		out.write(".output{");
+		out.write("	height: 50px;");
+		out.write("	background: #e9e9e9;");
+		out.write("	font-size: 24px;");
+		out.write("	font-weight: bold;");
+		out.write("	text-align: right;");
+		out.write("	padding: 0px 5px;");
+		out.write("}");
+		out.write("</style>");
 
-		
-		if(op.equals("=")) {  // "="이면 계산
-			int x = 0;
-			int y = num;
-			for( Cookie c : cookies ) {
-				if(c.getName().equals("value")) {
-					x = Integer.parseInt( c.getValue() );
-					break;
-				}
-			}
-			String operator = "";
-			for( Cookie c : cookies ) {
-				if(c.getName().equals("op")) {
-					operator = c.getValue();
-					break;
-				}
-			}
-			int result = 0;
-			if(operator.equals("+")) {
-				result = x+y;
-			} else {
-				result = x-y;
-			}
-			out.printf( "Result is %d", result );
-			
-		} else {  // "="이 아닐 경우 값 저장
-//			session.setAttribute("number", num);  // session 방식
-//			session.setAttribute("operator", op);
-			Cookie valueCookie = new Cookie("value", String.valueOf(num));  // Cookie는 String만 저장 가능
-			Cookie opCookie = new Cookie("op", op);
-			
-//			## setPath : 쿠키를 사용할 URL 지정. 하위 URL 요청 시에만 쿠키가 전달됨. 기본값은 루트("/")
-//			valueCookie.setPath("/");  // 모든 URL 요청 시 valueCookie 전달됨
-//			opCookie.setPath("/");	
-			
-			valueCookie.setMaxAge(24*60*60); // 만료기간 설정. 단위: 초. 설정 안하면 브라우저 종료 시 제거.
-			opCookie.setMaxAge(24*60*60);		
-			
-			response.addCookie(valueCookie);  // 쿠키 클라이언트에 저장
-			response.addCookie(opCookie);  
-			
-			response.sendRedirect("/calc4.html");  // 리다이렉션
-		}
+		out.write("</head>");
+		out.write("<body>");
+		out.write("	<form action=\"calc5\" method=\"post\">");
+		out.write("		<table>");
+		out.write("			<tr>");
+		out.write("				<td class=\"output\" colspan=\"4\">0</td>");
+		out.write("			</tr>");
+		out.write("			<tr>");
+		out.write("				<td><input type=\"submit\" name=\"operator\" value=\"CE\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"operator\" value=\"C\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"operator\" value=\"BS\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"operator\" value=\"÷\" /></td>");
+		out.write("			</tr>");
+		out.write("			<tr>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"7\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"8\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"9\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"operator\" value=\"X\" /></td>");
+		out.write("			</tr>");
+		out.write("			<tr>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"4\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"5\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"6\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"operator\" value=\"-\" /></td>");
+		out.write("			</tr>");
+		out.write("			<tr>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"1\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"2\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"3\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"operator\" value=\"+\" /></td>");
+		out.write("			</tr>");
+		out.write("			<tr>");
+		out.write("				<td></td>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"0\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"operator\" value=\".\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"operator\" value=\"=\" /></td>");
+		out.write("			</tr>");
+		out.write("		</table>");
+		out.write("	</form>");
+		out.write("</body>");
+		out.write("</html>");
 	}
 }
