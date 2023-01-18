88d03510a01a6519e5f29e546e4f8ffc99155460
stadia <stadia1012@gmail.com>
2023-01-19 02:55:46 +0900

    calculator - 파일 통합

---
+++ StadiaJSP/src/main/java/site/stadiajsp/servlet/Calculator.java
@@ -0,0 +1,156 @@
+package site.stadiajsp.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+// Session 객체를 이용한 값 저장
+
+@WebServlet("/calculator")
+public class Calculator extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+Cookie[] cookies = request.getCookies();
+		
+		String exp ="0";
+		if (cookies != null) {
+			for( Cookie c : cookies ) {
+				if(c.getName().equals("exp")) {
+					exp = c.getValue();
+					break;
+				}
+			}
+		}
+		
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		
+		out.write("<!DOCTYPE html>");
+		out.write("<html>");
+		out.write("<head>");
+		out.write("<meta charset=\"UTF-8\">");
+		out.write("<title>Stadia's Home</title>");
+		out.write("<style>");
+		out.write("input{");
+		out.write("	width: 50px;");
+		out.write("	height: 50px;");
+		out.write("}");
+		out.write(".output{");
+		out.write("	height: 50px;");
+		out.write("	background: #e9e9e9;");
+		out.write("	font-size: 24px;");
+		out.write("	font-weight: bold;");
+		out.write("	text-align: right;");
+		out.write("	padding: 0px 5px;");
+		out.write("}");
+		out.write("</style>");
+
+		out.write("</head>");
+		out.write("<body>");
+		out.write("	<form method=\"post\">");  // action 지정할 필요 없음. 같은 URL 이기에.
+		out.write("		<table>");
+		out.write("			<tr>");
+		out.printf("				<td class=\"output\" colspan=\"4\">%s</td>", exp);
+		out.write("			</tr>");
+		out.write("			<tr>");
+		out.write("				<td><input type=\"submit\" name=\"operator\" value=\"CE\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"operator\" value=\"C\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"operator\" value=\"BS\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"operator\" value=\"÷\" /></td>");
+		out.write("			</tr>");
+		out.write("			<tr>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"7\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"8\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"9\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"operator\" value=\"X\" /></td>");
+		out.write("			</tr>");
+		out.write("			<tr>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"4\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"5\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"6\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"operator\" value=\"-\" /></td>");
+		out.write("			</tr>");
+		out.write("			<tr>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"1\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"2\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"3\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"operator\" value=\"+\" /></td>");
+		out.write("			</tr>");
+		out.write("			<tr>");
+		out.write("				<td></td>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"0\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"operator\" value=\".\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"operator\" value=\"=\" /></td>");
+		out.write("			</tr>");
+		out.write("		</table>");
+		out.write("	</form>");
+		out.write("</body>");
+		out.write("</html>");
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+Cookie[] cookies = request.getCookies();
+		
+		String value = request.getParameter("value");
+		String operator = request.getParameter("operator");
+		String dot = request.getParameter("dot");
+		
+		String exp ="0"; 
+		if (cookies != null) {
+			for( Cookie c : cookies ) {
+				if(c.getName().equals("exp")) {
+					exp = c.getValue();
+					break;
+				}
+			}
+		}
+		
+		if (operator != null && operator.equals("÷")) {
+			operator = "/";
+		} else if (operator != null && operator.equals("X")) {
+			operator = "*";
+		} else if (operator != null && operator.equals("BS")) {
+			exp = exp.substring(0, exp.length()-1 );
+			operator = "=";
+		}
+		
+		if(operator != null && operator.equals("=")) {  // = 연산자는 계산. 나머지는 누적
+			ScriptEngine engine = new ScriptEngineManager().getEngineByName("graal.js"); // js엔진 사용
+			try {
+				exp = String.valueOf( engine.eval(exp) );
+				if (exp.equals("null")) exp = "0";
+			} catch (ScriptException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			exp += (value == null)?"":value;
+			exp += (operator == null)?"":operator;
+			exp += (dot == null)?"":operator;
+		}
+		
+		Cookie expCookie = new Cookie("exp", exp);
+		if(operator != null && operator.equals("C")) {  // 쿠키 삭제를 통한 계산기 값 초기화
+			expCookie.setMaxAge(0);
+		}
+		
+		expCookie.setPath("/calculator");  // 파일이 1개라서 쿠기 path 지정 가능. path는 1개만 지정 가능.
+		response.addCookie(expCookie);
+		response.sendRedirect("calculator");  // *get 요청임
+	}
+}
